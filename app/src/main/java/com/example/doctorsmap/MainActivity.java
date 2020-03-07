59d26b3d155a5423372038ab24fe5a18368b3c47
AyDevils <56445142+AyDevils@users.noreply.github.com>
2020-03-07 12:05:54 +0000

    Initial commit

---
+++ app/src/main/java/com/example/doctorsmap/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.doctorsmap;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap mapAPI;
+    SupportMapFragment mapFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapAPI);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mapAPI =googleMap;
+        LatLng London =new LatLng(51.532984, -0.131847);
+        mapAPI.addMarker(new MarkerOptions().position(London).title("London"));
+        mapAPI.moveCamera(CameraUpdateFactory.newLatLng(London));
+    }
+}
